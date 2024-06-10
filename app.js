97447f4d2a6e14ef3922c052d78490f9c182eecb
cleroux84 <62595571+cleroux84@users.noreply.github.com>
2024-06-10 09:21:28 +0200

    remove mailservicer

--- app.js
+++ app.js
@@ -57,7 +57,7 @@ let filePath = null;
 
 async function sendEmailWithAttachment(filePath, companyName, fileExtension, firstnameCustomer, nameCustomer, mailCustomer, phone) {
   const transporter = nodemailer.createTransport({
-      service: MAILSERVICE,
+
       host: MAILHOST,
       port: MAILPORT,
       secure: 'false',
