a3bd173f83e007aeaabffd27a1de2cfe1c8c630a
cleroux84 <cleroux84@gmail.com>
2024-06-10 15:03:18 +0200

    change cyphers again

--- app.js
+++ app.js
@@ -66,7 +66,7 @@ async function sendEmailWithAttachment(filePath, companyName, fileExtension, fir
           pass: MAILSENDERPASS
       },
       tls: {
-        ciphers: 'TLSv1.2'
+        ciphers: 'SSLv3'
     }
   });
 
