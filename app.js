06ba483f5c17413d84e209535eba5ce5ef01ce5e
cleroux84 <cleroux84@gmail.com>
2024-06-21 09:50:27 +0200

    create /updateOrder with log

--- app.js
+++ app.js
@@ -199,6 +199,11 @@ app.post('/proOrder', async (req, res) => {
   }
 });
 
+app.post('/updateOrder', (req, req) => {
+  const orderUpdated = req.body;
+  console.log("commande mise à jour: ", orderUpdated);
+})
+
 app.post('/updateKbis', (req, res) => {
   var updatedData = req.body;
   const clientUpdated = updatedData.id;
