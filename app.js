2de0ddcc0d6578f67a015dd9ea1049eb661f6b36
cleroux84 <cleroux84@gmail.com>
2024-06-21 11:12:43 +0200

    retropedalage

--- app.js
+++ app.js
@@ -60,82 +60,22 @@ app.get('/auth', (req, res) => {
   res.redirect(authUrl);
 });
 
-app.post('/get-token', async (req, res) => {
-  const code = req.body.code;
-  console.log('codegettoken', code);
-  try {         
-      const accessToken = await getAccessToken(code);
-      res.status(200).send('Access token obtained successfully');
-    } catch (error) {         
-      console.error('Error obtaining access token:', error);
-      res.status(500).send('Error obtaining access token');     
-    } });
-    
-    async function getAccessToken(code) {
-      console.log('codegetaccess', code);
-      try {
-        const response = await fetch('https://app.shippingbo.com/oauth/token', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json'
-          },
-          body: JSON.stringify({
-            grant_type: 'authorization_code',
-            code: YOUR_AUTHORIZATION_CODE,
-            redirect_uri: 'urn:ietf:wg:oauth:2.0:oob',
-            client_id: CLIENT_ID,
-            client_secret: CLIENT_SECRET
-          })
-        });
-     
-        const data = await response.json();
-        const responseText = await response.text();
-        console.log('response', responseText);
-        if (!response.ok) {
-          throw new Error(data.error || 'Failed to obtain access token');
-        }
-     
-        return data.access_token;
-      } catch (error) {
-        console.error('Error obtaining access token:', error);
-        throw error; // Réémettre l'erreur pour qu'elle soit capturée plus haut
-      }
-    }
 
-async function updateShippingboOrder(orderId, tags, accessToken) {
-  const shippingboApiUrl = `https://app.shippingbo.com/orders/${orderId}`;
-  const payload = { tags: tags };
-
-  const response = await fetch(shippingboApiUrl, {
-      method: 'PATCH',
-      headers: {
-          'Content-Type': 'application/json',
-          'x-api-version': 'v1',
-          'x-api-app-id': process.env.API_APP_ID,
-          'Authorization': `Bearer ${accessToken}`
-      },
-      body: JSON.stringify(payload)
-  });
-
-  if (!response.ok) {
-      const errorData = await response.json();
-      throw new Error(errorData.error || 'Failed to update order');
-  }
-}
 
 app.post('/updateOrder', async (req, res) => {
   const orderUpdated = req.body;
   const orderId = orderUpdated.id;
   const tags = orderUpdated.tags;
+  console.log('order updated', orderUpdated);
 
-  try {
-    const accessToken = await getAccessToken(YOUR_AUTHORIZATION_CODE);
-    await updateShippingboOrder(orderId, tags, accessToken);
-    res.status(200).send('Order updated successfully in Shippingbo');
-} catch (error) {
-    console.error('Error updating order in Shippingbo:', error);
-    res.status(500).send('Error updating order in Shippingbo');
-}
+//   try {
+//     const accessToken = await getAccessToken('');
+//     await updateShippingboOrder(orderId, tags, accessToken);
+//     res.status(200).send('Order updated successfully in Shippingbo');
+// } catch (error) {
+//     console.error('Error updating order in Shippingbo:', error);
+//     res.status(500).send('Error updating order in Shippingbo');
+// }
 
 });
 
