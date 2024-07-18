e538696e99dc5c9d1b49331eefc11a8c79ed54f3
cleroux84 <cleroux84@gmail.com>
2024-07-18 14:55:20 +0200

    refresh if no token in getToken

--- app.js
+++ app.js
@@ -142,8 +142,8 @@ const getToken = async (authorizationCode) => {
       accessToken = data.access_token;
       refreshToken = data.refresh_token;
       tokenExpiryTime = Date.now() + (data.expires_in * 1000);
-      await saveRefreshTokenDb(refreshToken);
     } else {
+      await saveRefreshTokenDb(refreshToken);
       console.log('gettoken no access but refreshtoken still:', refreshToken);
     }
     return {
