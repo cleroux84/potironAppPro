c3b2ea5780717ec608794eb5fd78622a7f969432
cleroux84 <cleroux84@gmail.com>
2024-09-11 10:56:55 +0200

    push notes quotes

--- app.js
+++ app.js
@@ -314,7 +314,7 @@ app.post('/updatedDraftOrder', async (req, res) => {
     deliveryAppointmentTag = "Rendez-vous : " + deliveryAppointmentValue;
     deliveryNotes = metafields.find(mf => mf.namespace === 'custom' && mf.key === 'palette_notes');
     deliveryNotesValue = deliveryNotes ? deliveryNotes.value : '';
-    const deliveryNotesEncoded = deliveryNotesValue.replace(/,/g, '&#44;');
+    const deliveryNotesEncoded = '"' + deliveryNotesValue + '"';
     deliveryNotesTag = 'Notes : ' + deliveryNotesEncoded;
   }
     if (isCompleted === true && isCommandePro) {
