80f47ba3eaeb324ae7b1d57b11b483a3af34a72a
cleroux84 <cleroux84@gmail.com>
2024-09-19 14:24:48 +0200

    first test to retrieve order by name66

--- services/shopifyApi.js
+++ services/shopifyApi.js
@@ -95,7 +95,7 @@ const createDraftOrder = async (draftOrder, accessToken) => {
 const orderById = async (orderName, orderMail) => {
   console.log("commande recherché", orderName);
   const orderTest = '7008'
-  const orderUrl = `https://potiron2021.myshopify.com/admin/api/2024-07/orders.json?name=${orderTest}`;
+  const orderUrl = `https://potiron2021.myshopify.com/admin/api/2024-07/orders.json?status=any&name=#7008`;
   const orderOptions = {
     method: 'GET',
     headers: {             
