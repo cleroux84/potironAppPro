74a02a17cf001cf54d53f482b7a35141b19e3808
cleroux84 <cleroux84@gmail.com>
2024-09-23 11:04:27 +0200

    log shipingmethod Colissimo 2

--- app.js
+++ app.js
@@ -488,7 +488,8 @@ app.get('/getOrderById', async (req, res) => {
   const customerId = req.query.customer_id;
   try {
     // const orderData = await orderById(orderName, orderMail, 6406535905430); // pas colissimo #8021
-    const orderData = await orderById(orderName, orderMail, 8074569285960); //1 colissimo #8058
+    const orderData = await orderById(orderName, orderMail, 8063057985864); //4 colissimo #8012
+    // const orderData = await orderById(orderName, orderMail, 8074569285960); //1 colissimo #8058
     // const orderData = await orderById(orderName, orderMail, customerId);
     // console.log("orderdata", orderData);
     // if(orderDetails.order.order_tags.includes('Commande PRO')) {
