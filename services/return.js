1736c4253c6def1d5b826cad144e92934e85708d
cleroux84 <cleroux84@gmail.com>
2024-09-24 09:31:35 +0200

    create price_rules with payload

--- services/return.js
+++ services/return.js
@@ -4,13 +4,6 @@ const fetch = require('node-fetch');
 
 const createDiscountCode = async (customerId, totalOrder) => {
     const createDiscountUrl = `https://potiron2021.myshopify.com/admin/api/2024-07/price_rules.json`
-    const createDiscountOptions = {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            'X-Shopify-Access-Token': SHOPIFYAPPTOKEN 
-        },
-    };
     const nowDate = new Date().toISOString();
     const OneWeekLater = new Date(Date.now() + 7 * 24 * 60 * 60 * 1000).toISOString();
     const discountTest = {
@@ -28,10 +21,27 @@ const createDiscountCode = async (customerId, totalOrder) => {
             "currency": "EUR"
          }
     }
+    const createDiscountOptions = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'X-Shopify-Access-Token': SHOPIFYAPPTOKEN 
+        },
+        body: JSON.stringify(discountTest)
+    };
+
     console.log("total", totalOrder);
-    const response = await fetch(createDiscountUrl, createDiscountOptions);
-    const data = await response.json();
-    console.log("created price rules", data);
+    try {
+        const response = await fetch(createDiscountUrl, createDiscountOptions);
+        if(!response.ok) {
+            console.log('error fetching price rules');
+        }
+        const data = await response.json();
+        console.log("created price rules", data);
+    } catch (error) {
+        console.error('erreur creation code de reduction');
+    }
+    
 }
 
 module.exports = {
