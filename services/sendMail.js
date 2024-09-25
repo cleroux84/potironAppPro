56ffdc0256c0147779482dc0c7e4d244470af2fd
cleroux84 <cleroux84@gmail.com>
2024-09-25 14:31:53 +0200

    set me to bonjour cancel

--- services/sendMail.js
+++ services/sendMail.js
@@ -71,7 +71,7 @@ async function sendMicrosoftEmailWithKbis(accessToken, filePath, companyName, fi
     };
     console.log('message ms365', message.toRecipients);
   try {
-      await client.api(`/users/bonjour@potiron.com/sendMail`).post({ message });
+      await client.api('/users/me/sendMail').post({ message });
       console.log('Email envoyé avec succès');
   } catch (error) {
       console.log('Erreur lors de l\'envoi de l\'email : ', error);
@@ -85,6 +85,7 @@ async function getMicrosoftAccessToken() {
 
   try {
       const authResponse = await cca.acquireTokenByClientCredential(clientCredentialRequest);
+      // console.log('m365accesstoken', authResponse.accessToken);
       return authResponse.accessToken;
   } catch (error) {
       console.log('Erreur d\'authentification : ', error);
