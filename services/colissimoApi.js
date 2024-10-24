b423870fa1ba19b7795adbca5bcd3c8050c25864
cleroux84 <cleroux84@gmail.com>
2024-10-24 15:46:14 +0200

    fetch url colissimo hard

--- services/colissimoApi.js
+++ services/colissimoApi.js
@@ -12,19 +12,51 @@ const createLabel = async (senderCustomer, recipientPotiron, parcel) => {
             "outputPrintingType": "PDF"
         },
         "letter": {
-            "sender": senderCustomer,
-            "addressee": recipientPotiron,
+            "service": {
+                "productCode": "DOM", // Exemple: code produit pour un envoi domestique
+                "depositDate": new Date().toISOString(), // Date actuelle pour l'envoi
+                "commercialName": "POTIRON TEST" // Nom commercial de l'expéditeur
+            },
             "parcel": {
                 "weight": parcel.weight,
-                "insuranceValue": 0
+                "insuranceValue": 0,
+                "recommendationLevel": "R1", // Niveau de recommandation (R1, R2, etc.)
+                "nonMachinable": false // Si le colis est non-mécanisable
+            },
+            "sender": {
+                "address": {
+                    "companyName": "client qui retourne",
+                    "lastName": "client qui retourne",
+                    "firstName": "client qui retourne",
+                    "line1": senderCustomer.address,
+                    "city": senderCustomer.city,
+                    "zipCode": senderCustomer.postalCode,
+                    "countryCode": senderCustomer.country,
+                    "email": "client qui retourne",
+                    "phoneNumber": "client qui retourne"
+                }
+            },
+            "addressee": {
+                "address": {
+                    "companyName": "Potiron",
+                    "lastName": "Céline",
+                    "firstName": "Céline",
+                    "line1": recipientPotiron.address,
+                    "city": recipientPotiron.city,
+                    "zipCode": recipientPotiron.postalCode,
+                    "countryCode": recipientPotiron.country,
+                    "email": "c.leroux@Potiron.com",
+                    "phoneNumber": "recipientPotiron.phoneNumber"
+                }
             }
         }
     };
-    const colissimoUrl = 'https://ws.colissimo.fr/sls-ws/SlsServiceWSRest/2.0/generateLabel';
+    const colissimoUrl = 'https://www.colissimo.fr/generateLabel';
     const colissimoOptions = {
         method: 'POST',
         headers: {
-            'Content-Type': 'application/json'
+            'Content-Type': 'application/json',
+            'apiKey': colissimoApiKey
             },
         body: JSON.stringify(data)
     }
