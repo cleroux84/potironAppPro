65e443e7e9a6c366bcae904b0dd85b5a214cd482
cleroux84 <cleroux84@gmail.com>
2024-10-30 11:41:03 +0100

    8294

--- app.js
+++ app.js
@@ -505,9 +505,8 @@ app.get('/getOrderById', async (req, res) => {
     // const orderData = await orderById(orderName, orderMail, 6406535905430); // pas colissimo #8021
     // const orderData = await orderById(orderName, orderMail, 8063057985864); //4 colissimo #8012
     // const orderData = await orderById(orderName, orderMail, 8074569285960); //1 colissimo #8058
-    // const orderData = await orderById(orderName, orderMail, 6261023539528); //6 colissimo #8295
-    const orderData = await orderById(orderName, orderMail, 6264550031688); //3 colissimo #8315
-        
+    const orderData = await orderById(orderName, orderMail, 8174393917768); //4 colissimo #8294
+    
     // const orderData = await orderById(orderName, orderMail, customerId); //moi livré : #6989
     // console.log("orderdata", orderData);
     
