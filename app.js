840a3e0729dd2270360814b008dfa22e1929a81b
cleroux84 <cleroux84@gmail.com>
2024-11-15 11:37:03 +0100

    retro2

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ const { getWarehouseOrderDetails, updateWarehouseOrder, getWarehouseOrderToRetur
 const { sendEmailWithKbis, sendWelcomeMailPro, sendReturnDataToCustomer, sendReturnDataToSAV } = require('./services/sendMail.js');
 const { createDraftOrder, getCustomerMetafields, updateProCustomer, createProCustomer, deleteMetafield, updateDraftOrderWithDraftId, lastDraftOrder, draftOrderById, orderById, getProductDetails, getProductWeightBySku, updateOrder, getOrderByShopifyId } = require('./services/shopifyApi.js');
 const { createReturnOrder, updateReturnOrder, checkIfPriceRuleExists, createPriceRule, isReturnableDate } = require('./services/return.js');
-const { refreshMS365AccessToken, getAccessTokenMS365, getAccessTokenMS365 } = require('./services/microsoftAuth.js');
+const { refreshMS365AccessToken, getAccessTokenMS365 } = require('./services/microsoftAuth.js');
 const { createLabel } = require('./services/colissimoApi.js');
 const { setupShippingboWebhook, deleteAllWebhooks, getWebhooks } = require('./services/shippingbo/webhooks.js');
 const { initializeTokens } = require('./services/manageTokens.js');
