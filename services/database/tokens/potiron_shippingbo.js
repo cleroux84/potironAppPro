a7b9752c87c2617f7676ed46d26d22d110bab77c
cleroux84 <cleroux84@gmail.com>
2024-11-21 15:19:36 +0100

    split potiron shippingbo in DB

---
+++ services/database/tokens/potiron_shippingbo.js
@@ -0,0 +1,42 @@
+//Requests for tokens table concerning Potiron Shippingbo
+const client = require('../db.js');
+
+// Get access token from DB
+const getAccessTokenFromDb = async () => {
+    try {
+      const res = await client.query('SELECT token FROM tokens LIMIT 1');
+      return res.rows[0].token;
+    } catch (error) {
+      console.log('Error retrieving token from db', error);
+      return null;
+    }
+  }
+
+  
+//Get refresh token from DB
+const getRefreshTokenFromDb = async () => {
+    try {
+      const res = await client.query('SELECT refresh_token FROM tokens LIMIT 1');
+      return res.rows[0].refresh_token;
+    } catch (error) {
+      console.log('Error retrieving refresh token', error);
+      return null;
+    }
+  }
+
+  const saveAccessAndRefreshTokenDb = async (token, refreshToken) => {
+    try {
+      await client.query('UPDATE tokens SET refresh_token = $1 WHERE id = 1', [refreshToken]);
+      console.log('RefreshToken saved in db for Potiron Paris', refreshToken);
+      await client.query('UPDATE tokens SET token = $1 WHERE id = 1', [token]);
+      console.log('token saved in db for Potiron Paris', token);
+    } catch (error) {
+      console.error('Error saving refreshToken in db', error);
+    }
+  }
+
+  module.exports = {
+    getAccessTokenFromDb,
+    getRefreshTokenFromDb,
+    saveAccessAndRefreshTokenDb
+  }
--- services/shippingbo/potironParisAuth.js
+++ services/shippingbo/potironParisAuth.js
@@ -1,40 +1,41 @@
 const client = require('../database/db.js');
 const fetch = require('node-fetch');
+const { getRefreshTokenFromDb, saveAccessAndRefreshTokenDb } = require('../database/tokens/potiron_shippingbo.js');
 const CLIENT_ID = process.env.CLIENT_ID_SHIPPINGBO;
 const CLIENT_SECRET = process.env.CLIENT_SECRET_SHIPPINGBO;
 let accessToken = null;
 let refreshToken = null;
 
-const saveRefreshTokenDb = async (token, refreshToken) => {
-    try {
-      await client.query('UPDATE tokens SET refresh_token = $1 WHERE id = 1', [refreshToken]);
-      console.log('RefreshToken saved in db for Potiron Paris', refreshToken);
-      await client.query('UPDATE tokens SET token = $1 WHERE id = 1', [token]);
-      console.log('token saved in db for Potiron Paris', token);
-    } catch (error) {
-      console.error('Error saving refreshToken in db', error);
-    }
-  }
+// const saveRefreshTokenDb = async (token, refreshToken) => {
+//     try {
+//       await client.query('UPDATE tokens SET refresh_token = $1 WHERE id = 1', [refreshToken]);
+//       console.log('RefreshToken saved in db for Potiron Paris', refreshToken);
+//       await client.query('UPDATE tokens SET token = $1 WHERE id = 1', [token]);
+//       console.log('token saved in db for Potiron Paris', token);
+//     } catch (error) {
+//       console.error('Error saving refreshToken in db', error);
+//     }
+//   }
 
-  const getRefreshTokenFromDb = async () => {
-    try {
-      const res = await client.query('SELECT refresh_token FROM tokens LIMIT 1');
-      return res.rows[0].refresh_token;
-    } catch (error) {
-      console.log('Error retrieving refresh token', error);
-      return null;
-    }
-  }
+  // const getRefreshTokenFromDb = async () => {
+  //   try {
+  //     const res = await client.query('SELECT refresh_token FROM tokens LIMIT 1');
+  //     return res.rows[0].refresh_token;
+  //   } catch (error) {
+  //     console.log('Error retrieving refresh token', error);
+  //     return null;
+  //   }
+  // }
 
-  const getAccessTokenFromDb = async () => {
-    try {
-      const res = await client.query('SELECT token FROM tokens LIMIT 1');
-      return res.rows[0].token;
-    } catch (error) {
-      console.log('Error retrieving token from db', error);
-      return null;
-    }
-  }
+  // const getAccessTokenFromDb = async () => {
+  //   try {
+  //     const res = await client.query('SELECT token FROM tokens LIMIT 1');
+  //     return res.rows[0].token;
+  //   } catch (error) {
+  //     console.log('Error retrieving token from db', error);
+  //     return null;
+  //   }
+  // }
 
   const getToken = async (authorizationCode) => {
     const tokenUrl = 'https://oauth.shippingbo.com/oauth/token';
@@ -63,7 +64,7 @@ const saveRefreshTokenDb = async (token, refreshToken) => {
         accessToken = data.access_token;
         refreshToken = data.refresh_token;
         console.log("getToken with auhorizationCode");
-        await saveRefreshTokenDb(accessToken, refreshToken);
+        await saveAccessAndRefreshTokenDb(accessToken, refreshToken);
       }
       return {
         accessToken,
@@ -75,11 +76,7 @@ const saveRefreshTokenDb = async (token, refreshToken) => {
   };
 
   const refreshAccessToken = async () => {
-    console.log('refresh before refresh', refreshToken);
-    console.log('token before refresh', accessToken);
     refreshToken = await getRefreshTokenFromDb();
-    console.log('refresh after refresh', refreshToken);
-    console.log('token after refresh', accessToken);
     const refreshUrl = 'https://oauth.shippingbo.com/oauth/token';
     const refreshOptions = {
       method: 'POST',
@@ -102,7 +99,7 @@ const saveRefreshTokenDb = async (token, refreshToken) => {
         accessToken = data.access_token;
         refreshToken = data.refresh_token;
         console.log('BUG NEW ACCESS', data)
-        await saveRefreshTokenDb(accessToken, refreshToken);
+        await saveAccessAndRefreshTokenDb(accessToken, refreshToken);
       } else {
         console.error('refresh failed here', data)
       }
@@ -113,6 +110,5 @@ const saveRefreshTokenDb = async (token, refreshToken) => {
   
   module.exports = {
     getToken,
-    refreshAccessToken,
-    getAccessTokenFromDb
+    refreshAccessToken
   }
