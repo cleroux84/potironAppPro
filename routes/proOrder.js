7813c66ba910f28cb86cdb72259ac9801bc0ec99
cleroux84 <cleroux84@gmail.com>
2024-12-05 10:15:09 +0100

    update draft warehouse 5min 3

--- routes/proOrder.js
+++ routes/proOrder.js
@@ -14,7 +14,7 @@ const { getCustomerMetafields } = require('../services/API/Shopify/customers');
 router.post('/updateDraftOrder', async (req, res) => {
   const createdOrder= req.body;
   let accessTokenWarehouse = getAccessTokenWarehouseFromDb();
-  if(createdOrder.object.origin === 'Potironpro' && (createdOrder.object.origin_ref).includes('provisoire') && createdOrder.object.state !== "to_be_prepared") {
+  if(createdOrder.object.origin === 'Potironpro' && (createdOrder.object.origin_ref).includes('provisoire') && createdOrder.object.state === "to_be_prepared") {
     console.log('order update waiting_for_payment in 5 minutes', createdOrder.object.id);
     let shippingboId = createdOrder.object.id; 
     setTimeout (updateWarehouseOrderPayments, 5 * 60 * 1000, accessTokenWarehouse, shippingboId);
