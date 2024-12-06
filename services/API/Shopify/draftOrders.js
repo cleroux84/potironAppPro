48cbcb49f9adb5013fd86aee49ea5dbc5e4d304b
cleroux84 <cleroux84@gmail.com>
2024-12-06 10:29:37 +0100

    test for headers3

--- services/API/Shopify/draftOrders.js
+++ services/API/Shopify/draftOrders.js
@@ -88,7 +88,7 @@ const createDraftOrder = async (draftOrder, accessToken) => {
         shipping_address_id: data.draft_order.shipping_address.id,
         source: 'Potironpro',
         source_ref: draftOrderId,
-        state: 'waiting_for_payment',
+        state: 'dispatched',
         total_price_cents: data.draft_order.subtotal_price * 100,
         total_price_currency: 'EUR',
         tags_to_add: dataForShippingboTag
