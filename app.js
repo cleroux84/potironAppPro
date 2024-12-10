a660d2e399da75924a5dd73e3da2c2a7d7695179
cleroux84 <cleroux84@gmail.com>
2024-12-10 09:26:30 +0100

    fix end date 9h30

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ getWebhooks();
 
 //CHECK Scheduled emails in DB every day
 // cron.schedule('0 9 * * *', checkScheduledEmails, { //9h00
-cron.schedule('06 9 * * *', checkScheduledEmails, { //10h50
+cron.schedule('30 9 * * *', checkScheduledEmails, { //10h50
   schedule: true,
   timezone: "Europe/Paris"
 });
