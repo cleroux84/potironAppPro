0ad5a62154bc9570a0e30d43e518c12b2a9494af
cleroux84 <cleroux84@gmail.com>
2025-01-15 16:29:29 +0100

    test route returnContact

--- routes/returnOrder.js
+++ routes/returnOrder.js
@@ -15,7 +15,16 @@ const { createLabel, getShippingPrice, calculateTotalShippingCost, getGroupedIte
 const { getProductWeightBySku } = require('../services/API/Shopify/products');
 const { checkIfReturnOrderExist, createReturnOrder } = require('../services/API/Shippingbo/Gma/returnOrdersCRUD');
 const { sendReturnDataToSAV, sendRefundDataToSAV, mailToSendRefund } = require('../services/sendMails/mailForTeam');
+const { post } = require('./returnOrder');
 const router = express.Router();
+
+//give data for return order demand with contact form
+router.post('/returnContact', (req, res) => {
+  const {orderWarehouse, orderShopify, items} = req.body;
+  console.log('warehouse', orderWarehouse);
+  console.log('items to return', items);
+  console.log('here data to save in new db table')
+})
 
 //trigger on shippingbo webhook (cancel order / will become returned ?) to create and send discount code to customer
 router.post('/returnOrderCancel', async (req, res) => {
