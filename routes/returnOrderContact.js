72dcf6b53efe6a77bf519fef34f2301bb5c5149f
cleroux84 <cleroux84@gmail.com>
2025-01-16 12:13:15 +0100

    extract data enricheditems

--- routes/returnOrderContact.js
+++ routes/returnOrderContact.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const { getReturnContactData } = require('../services/database/return_contact');
 const { getOrderByShopifyId } = require('../services/API/Shopify/orders');
+const { getProductWeightBySku } = require('../services/API/Shopify/products');
 const router = express.Router();
 
 router.get('/returnForm:id', async (req, res) => {
@@ -14,6 +15,26 @@ router.get('/returnForm:id', async (req, res) => {
         orderCreatedAt: shopifyOrder.order.created_at,
         fullName: shopifyOrder.order.customer.first_name + shopifyOrder.order.customer.last_name 
     }
+
+    const items = returnDataFromDb.items;
+    const enrichItemsWithData = async (items) => {
+        const enrichedItems = [];
+        for(const item of items) {
+            const productDetails = await getProductWeightBySku(item.product_user_ref);
+            if(productDetails) {
+                enrichedItems.push({
+                    ...item,
+                    title: productDetails.title,
+                    imageUrl: productDetails.imageUrl
+                });
+            } else {
+                console.error('Details du produit non trouvé')
+            }
+        console.log('enrichedItems', enrichedItems);
+        }
+    }
+    enrichItemsWithData(items);
+
 
     console.log('dataCustomer', dataCustomer);
 })
