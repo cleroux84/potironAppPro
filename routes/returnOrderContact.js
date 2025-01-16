55cea20cb486f3216be3c4b4775ea33656ea0e03
cleroux84 <cleroux84@gmail.com>
2025-01-16 11:50:05 +0100

    extract data

--- routes/returnOrderContact.js
+++ routes/returnOrderContact.js
@@ -14,24 +14,8 @@ router.get('/returnForm:id', async (req, res) => {
         orderCreatedAt: shopifyOrder.order.created_at,
         fullName: shopifyOrder.order.customer.first_name + shopifyOrder.order.customer.last_name 
     }
-    const orderItems = returnDataFromDb.items_to_return;
-    const enrichOrderItems = async (orderItems) => {
-        const enrichedItems = await Promise.all(orderItems.map(async (item) => {
-          const sku = item.product_ref; 
-          const priceData = lineItemsMapping[sku] || { price: null };
-          const productVariant = await getProductWeightBySku(sku);
-  
-          return {
-            ...item,
-            price: priceData.price,
-            imageUrl: productVariant?.product?.featuredImage?.url || null, 
-          };
-        }));
-      return enrichedItems;
-    };
-    const enrichedOrderItems = await enrichOrderItems(orderItems);
+
     console.log('dataCustomer', dataCustomer);
-    console.log('enrichItems', enrichedOrderItems);
 })
 
 module.exports = router;
