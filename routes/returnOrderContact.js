56ca2ee6ff6cd3c0c64e0e284f44419c110a4387
cleroux84 <cleroux84@gmail.com>
2025-01-16 11:34:34 +0100

    getOrder

--- routes/returnOrderContact.js
+++ routes/returnOrderContact.js
@@ -1,9 +1,14 @@
 const express = require('express');
+const { getReturnContactData } = require('../services/database/return_contact');
+const { getOrderByShopifyId } = require('../services/API/Shopify/orders');
 const router = express.Router();
 
 router.get('/returnForm:id', async (req, res) => {
     const { id } = req.params;
-    console.log('id to find in db', id);
+    const returnDataFromDb = await getReturnContactData(id);
+    const shopifyOrder = await getOrderByShopifyId(returnDataFromDb.shopify_id);
+    console.log('returnData', returnDataFromDb);
+    console.log('shopify', shopifyOrder);
 })
 
 module.exports = router;
