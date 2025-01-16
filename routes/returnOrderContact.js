be558c9b9f63778994462df3c56bb519929c82cf
cleroux84 <cleroux84@gmail.com>
2025-01-16 11:44:24 +0100

    extract data from db and functions

--- routes/returnOrderContact.js
+++ routes/returnOrderContact.js
@@ -7,8 +7,31 @@ router.get('/returnForm:id', async (req, res) => {
     const { id } = req.params;
     const returnDataFromDb = await getReturnContactData(id);
     const shopifyOrder = await getOrderByShopifyId(returnDataFromDb.shopify_id);
-    console.log('returnData', returnDataFromDb);
-    console.log('shopify', shopifyOrder);
+
+    const dataCustomer = {
+        orderName : shopifyOrder.order.name,
+        customerMail : shopifyOrder.order.email,
+        orderCreatedAt: shopifyOrder.order.created_at,
+        fullName: shopifyOrder.order.customer.first_name + shopifyOrder.order.customer.last_name 
+    }
+    const orderItems = returnDataFromDb.items_to_return;
+    const enrichOrderItems = async (orderItems) => {
+        const enrichedItems = await Promise.all(orderItems.map(async (item) => {
+          const sku = item.product_ref; 
+          const priceData = lineItemsMapping[sku] || { price: null };
+          const productVariant = await getProductWeightBySku(sku);
+  
+          return {
+            ...item,
+            price: priceData.price,
+            imageUrl: productVariant?.product?.featuredImage?.url || null, 
+          };
+        }));
+      return enrichedItems;
+    };
+    const enrichedOrderItems = await enrichOrderItems(orderItems);
+    console.log('dataCustomer', dataCustomer);
+    console.log('enrichItems', enrichedOrderItems);
 })
 
 module.exports = router;
