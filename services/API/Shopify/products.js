8e674607687cd0d50b9ef80fc42a7461297016fd
cleroux84 <cleroux84@gmail.com>
2025-01-29 14:18:32 +0100

    retry

--- services/API/Shopify/products.js
+++ services/API/Shopify/products.js
@@ -9,60 +9,56 @@ const fetch = require('node-fetch');
 const getProductWeightBySku = async (sku) => {
   const getProductDetailsUrl = 'https://potiron2021.myshopify.com/admin/api/2024-07/graphql.json';
   const getProductDetailsOptions = {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      "X-Shopify-Access-Token": SHOPIFYAPPTOKEN,
-    },
-    body: JSON.stringify({
-      query: `
-      {
-        productVariants(first: 1, query: "sku:${sku}") {
-          edges {
-            node {
-              id
-              sku
-              product {
-                id
-                title
-                variants {
-                  id
-                  sku
-                  inventoryItem {
-                    measurement {
-                      weight {
-                        unit
-                        value
+      method: "POST",
+      headers: {
+          "Content-Type": "application/json",
+          "X-Shopify-Access-Token": SHOPIFYAPPTOKEN,
+      },
+      body: JSON.stringify({
+          query: `
+          {
+              productVariants(first: 1, query: "sku:${sku}") {
+                  edges {
+                      node {
+                          id
+                          sku
+                          inventoryItem {
+                              measurement {
+                                  weight {
+                                      unit
+                                      value
+                                  }
+                              }
+                          }
+                          product {
+                              id
+                              title
+                          }
                       }
-                    }
                   }
-                }
               }
-            }
           }
-        }
-      }
-    `,
-    }),
+          `,
+      }),
   };
 
   try {
-    const response = await fetch(getProductDetailsUrl, getProductDetailsOptions);
-    if (!response.ok) throw new Error(`Erreur lors de la récupération du produit par SKU : ${response.statusText}`);
-    const data = await response.json();
-    // Affiche la réponse complète pour le débogage
-    console.log("Réponse de l'API :", data);
+      const response = await fetch(getProductDetailsUrl, getProductDetailsOptions);
+      if (!response.ok) throw new Error(`Erreur lors de la récupération du produit par SKU : ${response.statusText}`);
 
-    const productVariant = data.data?.productVariants?.edges[0]?.node;
-    if (!productVariant) {
-      console.log("Aucun produit trouvé pour ce SKU");
-      return null;
-    }
+      const data = await response.json();
+      console.log("Réponse de l'API :", data);
 
-    // console.log("Produit trouvé :", productVariant);
-    return productVariant;
+      const productVariant = data.data?.productVariants?.edges[0]?.node;
+
+      if (!productVariant) {
+          console.log("Aucun produit trouvé pour ce SKU");
+          return null;
+      }
+
+      return productVariant;
   } catch (error) {
-    console.error("Erreur lors de la récupération du produit par SKU :", error);
+      console.error("Erreur lors de la récupération du produit par SKU :", error);
   }
 };
 
