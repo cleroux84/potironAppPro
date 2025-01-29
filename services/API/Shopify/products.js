0a90786eeb5b67e7c6497fbbfc46fd0e8294da6b
cleroux84 <cleroux84@gmail.com>
2025-01-29 11:33:05 +0100

    update getProductWeightBySku for graphiql shopify changements

--- services/API/Shopify/products.js
+++ services/API/Shopify/products.js
@@ -7,55 +7,57 @@ const fetch = require('node-fetch');
 
 //Get a product by Sku (PP-)
 const getProductWeightBySku = async (sku) => {
-    const getProductDetailsUrl = 'https://potiron2021.myshopify.com/admin/api/2024-04/graphql.json';
-    const getProductDetailsOptions = {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        "X-Shopify-Access-Token": SHOPIFYAPPTOKEN,
-      },
-      body: JSON.stringify({
-        query: `
-          {
-            productVariants(first: 1, query: "sku:${sku}") {
-              edges {
-                node {
+  const getProductDetailsUrl = 'https://potiron2021.myshopify.com/admin/api/2024-07/graphql.json';
+  const getProductDetailsOptions = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "X-Shopify-Access-Token": SHOPIFYAPPTOKEN,
+    },
+    body: JSON.stringify({
+      query: `
+        {
+          productVariants(first: 1, query: "sku:${sku}") {
+            edges {
+              node {
+                id
+                sku
+                price
+                inventoryItem {
                   id
-                  sku
                   weight
-                  price
-                  product {
-                    id
-                    title
-                    featuredImage {
-                      url
-                    }
+                  weightUnit
+                }
+                product {
+                  id
+                  title
+                  featuredImage {
+                    url
                   }
                 }
               }
             }
           }
-        `,
-      }),
-    };
-   
-    try {
-      const response = await fetch(getProductDetailsUrl, getProductDetailsOptions);
-      if (!response.ok) throw new Error(`Erreur lors de la récupération du produit par SKU : ${response.statusText}`);
-      const data = await response.json();
-      const productVariant = data.data.productVariants.edges[0]?.node;
-      if (!productVariant) {
-        console.log("Aucun produit trouvé pour ce SKU");
-        return null;
-      }
-   
-      // console.log("Produit trouvé :", productVariant);
-      return productVariant;
-    } catch (error) {
-      console.error("Erreur lors de la récupération du produit par SKU :", error);
-    }
+        }
+      `,
+    }),
   };
-
-  module.exports = {
-    getProductWeightBySku
+  try {
+    const response = await fetch(getProductDetailsUrl, getProductDetailsOptions);
+    if (!response.ok) throw new Error(`Erreur lors de la récupération du produit par SKU : ${response.statusText}`);
+    const data = await response.json();
+    const productVariant = data.data.productVariants.edges[0]?.node;
+    if (!productVariant) {
+      console.log("Aucun produit trouvé pour ce SKU");
+      return null;
+    }
+    // console.log("Produit trouvé :", productVariant);
+    return productVariant;
+  } catch (error) {
+    console.error("Erreur lors de la récupération du produit par SKU :", error);
   }
+};
+
+module.exports = {
+  getProductWeightBySku
+};
