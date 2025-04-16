456c61b8240765308e9137bf6127e5d8c55c3507
cleroux84 <cleroux84@gmail.com>
2025-04-16 15:19:47 +0200

    conditions to send invoice

--- routes/allOrders.js
+++ routes/allOrders.js
@@ -3,7 +3,11 @@ const router = express.Router();
 
 router.post('/sendInvoice', async (req, res) => {
     const newOrder = req.body;
-    console.log('new order Shippingbo', newOrder);
+    if(newOrder.additional_data.from === 'dispatched'
+        && newOrder.additional_data.to === 'in_preparation'
+        && newOrder.object.origin === 'POTIRON.COM') {
+        console.log('send invoice for : ', newOrder.object.id);
+    }
 }) 
 
 module.exports = router;
