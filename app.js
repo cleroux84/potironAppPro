29fcc4292aaf2033825a911124c857f56f5eb575
cleroux84 <cleroux84@gmail.com>
2025-05-15 16:33:24 +0200

    test new afibel orders

--- app.js
+++ app.js
@@ -62,13 +62,13 @@ getWebhooks();
 //   });
 
 
-// cron.schedule('10 16  * * *', () => {
-//   console.log("⏰ getNewOrdersFile déclenché à : ", new Date().toLocaleString('fr-FR', { timeZone: 'Europe/Paris' }));
-//   // getNewOrdersFile();
-//   generateCsv();
-// }, {
-//   timezone: "Europe/Paris"
-// });
+cron.schedule('38 16  * * *', () => {
+  console.log("⏰ getNewOrdersFile déclenché à : ", new Date().toLocaleString('fr-FR', { timeZone: 'Europe/Paris' }));
+  getNewOrdersFile();
+  // generateCsv();
+}, {
+  timezone: "Europe/Paris"
+});
 
 cron.schedule('0 9 * * *', () => {
   console.log("⏰ checkScheduledEmails déclenché à : ", new Date().toLocaleString('fr-FR', { timeZone: 'Europe/Paris' }));
