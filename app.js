a2cfd803abcdf400fce2ad3bd880862f73210180
cleroux84 <cleroux84@gmail.com>
2025-06-04 15:44:24 +0200

    translation2

--- app.js
+++ app.js
@@ -62,7 +62,7 @@ getWebhooks();
 //   });
 
 
-cron.schedule('07 15  * * *', () => {
+cron.schedule('47 15  * * *', () => {
   console.log("⏰ getNewOrdersFile déclenché à : ", new Date().toLocaleString('fr-FR', { timeZone: 'Europe/Paris' }));
   // getNewOrdersFile();
   generateCsv();
