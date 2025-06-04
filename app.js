41b57e2092779ce364ef9792fddf3c9129b44577
cleroux84 <cleroux84@gmail.com>
2025-06-04 14:33:19 +0200

    test tracking name with date afibel2

--- app.js
+++ app.js
@@ -64,8 +64,8 @@ getWebhooks();
 
 cron.schedule('35 14  * * *', () => {
   console.log("⏰ getNewOrdersFile déclenché à : ", new Date().toLocaleString('fr-FR', { timeZone: 'Europe/Paris' }));
-  getNewOrdersFile();
-  // generateCsv();
+  // getNewOrdersFile();
+  generateCsv();
 }, {
   timezone: "Europe/Paris"
 });
