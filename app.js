59f7db1fed69dae67f0c89e5229d886b97daf720
cleroux84 <cleroux84@gmail.com>
2025-06-20 16:22:55 +0200

    test afibel 16:30

--- app.js
+++ app.js
@@ -62,7 +62,7 @@ getWebhooks();
 //   });
 
 
-cron.schedule('15 16  * * *', () => {
+cron.schedule('27 16  * * *', () => {
   console.log("⏰ getNewOrdersFile déclenché à : ", new Date().toLocaleString('fr-FR', { timeZone: 'Europe/Paris' }));
   getNewOrdersFile();
   // generateCsv();
