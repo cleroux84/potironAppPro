d09101ef4a3b01c9c981cd4732a0b23b6bd81e84
cleroux84 <cleroux84@gmail.com>
2025-06-25 12:02:08 +0200

    rm test 12:5

--- app.js
+++ app.js
@@ -62,7 +62,7 @@ getWebhooks();
 //   });
 
 
-cron.schedule('56 11  * * *', () => {
+cron.schedule('5 12  * * *', () => {
   console.log("⏰ getNewOrdersFile déclenché à : ", new Date().toLocaleString('fr-FR', { timeZone: 'Europe/Paris' }));
   getNewOrdersFile();
   // generateCsv();
