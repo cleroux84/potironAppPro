47eda19c8c3b15408c41c79b1d4e917474fe4652
cleroux84 <cleroux84@gmail.com>
2025-12-17 14:27:30 +0100

    update customer

--- routes/gmaReassortRoute.js
+++ routes/gmaReassortRoute.js
@@ -54,4 +54,44 @@ router.post('/createCustomer', async (req, res) => {
     res.status(200).json(updatedCustomerData);
 }) 
 
+// webhook on customer update from gma-reassort
+router.post('/updateCustomer', async (req, res) => {
+    var data = req.body;
+    // console.log('data:', data);
+    const clientToUpdate = data.id;
+    const company = extractInfoFromNote(data.note, 'Entreprise');
+    const siret = extractInfoFromNote(data.note, 'Siret');
+    const tva = extractInfoFromNote(data.note, 'TVA');
+
+    const updatedCustomer = {
+        customer: {
+            id: clientToUpdate,
+            note: '',
+            metafields: [
+                {
+                    key: 'company',
+                    value: company,
+                    type: 'single_line_text_field',
+                    namespace: 'custom'
+                },
+                {
+                    key: 'siret',
+                    value: siret,
+                    type: 'single_line_text_field',
+                    namespace: 'custom'
+                },
+                {
+                    key: 'tva',
+                    value: tva,
+                    type: 'single_line_text_field',
+                    namespace: 'custom'
+                },
+            ]
+        }
+    };
+    const updatedCustomerData = await createMetaCustomer(clientToUpdate, updatedCustomer);
+    // console.log('client update', updatedCustomerData);
+    res.status(200).json(updatedCustomerData);
+}) 
+
 module.exports = router;
