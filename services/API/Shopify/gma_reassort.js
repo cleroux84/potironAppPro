a974c7503ecf19b995520ef01c87883f3445698b
cleroux84 <cleroux84@gmail.com>
2025-12-19 15:17:34 +0100

    puppeteer3

--- services/API/Shopify/gma_reassort.js
+++ services/API/Shopify/gma_reassort.js
@@ -103,8 +103,18 @@ const generateInvoicePdf = async(invoiceData, outpath = 'invoice.pdf') => {
     const template = Handlebars.compile(templateHtml);
     const finalHtml = template(invoiceData);
     const browser = await puppeteer.launch({
-    headless: true,
-    args: ['--no-sandbox', '--disable-setuid-sandbox']
+        headless: true,
+        executablePath: process.env.PUPPETEER_EXECUTABLE_PATH || '/usr/bin/chromium-browser',
+        args: [
+            '--no-sandbox',
+            '--disable-setuid-sandbox',
+            '--disable-dev-shm-usage',
+            '--disable-accelerated-2d-canvas',
+            '--no-first-run',
+            '--no-zygote',
+            '--single-process',
+            '--disable-gpu'
+        ]
     });
     const page = await browser.newPage();
     await page.setContent(finalHtml, { waitUntil: 'networkidle0' });
