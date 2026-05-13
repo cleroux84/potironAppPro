ec24644c2d4b182b51f08deae85c3b3937262d44
cleroux84 <cleroux84@gmail.com>
2026-05-13 10:29:29 +0200

    test afibel

--- app.js
+++ app.js
@@ -86,7 +86,7 @@ app.get('/callback', async (req, res) => {
     }
 });
 
-cron.schedule('22 10  * * *', () => {
+cron.schedule('32 10  * * *', () => {
   console.log("⏰ generateCsv déclenché à : ", new Date().toLocaleString('fr-FR', { timeZone: 'Europe/Paris' }));
   generateCsv();
 }, {
